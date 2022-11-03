05116633e553e706da6b48efc504ba17550d044d
Paweł Ostrowski <pasza@pasza.org>
2022-11-03 21:58:05 +0100

    neovim treesitter syntax highlighting

--- .config/nvim/lua/pasza/packer.lua
+++ .config/nvim/lua/pasza/packer.lua
@@ -6,5 +6,14 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
+  use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+  }
 end)
 
