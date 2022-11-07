83fe11b940c7c1c4b2e9d02ca7dcdbcc391da055
Paweł Ostrowski <pasza@pasza.org>
2022-11-07 02:14:59 +0100

    working custom on_attach for jdtls

--- .config/nvim/lua/pasza/jj.lua
+++ .config/nvim/lua/pasza/jj.lua
@@ -1,8 +1,41 @@
 local JJ = {}
 -- require("lspconfig").jdtls.setup({})
+
+
+local custom_attach = function(client, bufnr)
+    require('jdtls.setup').add_commands()
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    local opts = { noremap=true, silent=true }
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references() && vim.cmd("copen")<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    -- Java specific
+    buf_set_keymap("n", "<leader>di", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+    buf_set_keymap("n", "<leader>dt", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
+    buf_set_keymap("n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
+    buf_set_keymap("v", "<leader>de", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+    buf_set_keymap("n", "<leader>de", "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+    buf_set_keymap("v", "<leader>dm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+end
+
 local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  on_attach = custom_attach,
   cmd = {
 
     -- 💀
@@ -16,12 +49,12 @@ local config = {
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-javaagent:/home/ttt/.m2/repository/org/projectlombok/lombok/1.18.24/lombok-1.18.24.jar',
+--    '-javaagent:/home/pasza/.m2/repository/org/projectlombok/lombok/1.18.24/lombok-1.18.24.jar',
 
     -- 💀
-    '-jar', '/home/ttt/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-    '-configuration', '/home/ttt/.local/share/nvim/mason/packages/jdtls/config_linux',
-    '-data', '/home/ttt/tmp/dev/java'
+    '-jar', '/home/pasza/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+    '-configuration', '/home/pasza/.local/share/nvim/mason/packages/jdtls/config_linux',
+    '-data', '/home/pasza/tmp/dev/java'
   },
 
   -- 💀
@@ -38,13 +71,13 @@ local config = {
   }
 }
 --local config = {
---    cmd = {'/home/ttt/.local/share/nvim/mason/bin/jdtls' },
+--    cmd = {'/home/pasza/.local/share/nvim/mason/bin/jdtls' },
 --    root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 --}
 
 JJ.jjattach = function()
     require('jdtls').start_or_attach(config)
-    require('jdtls.setup').add_commands()
+    --require('jdtls.setup').add_commands()
 end
 
 return JJ
