7b662d095db39619b0b2b3bb801b2e0b5782e475
Paweł Ostrowski <pasza@pasza.org>
2022-12-03 18:00:36 +0100

    nvim: center view after C-d, C-u and search

--- .config/nvim/lua/user/remap.lua
+++ .config/nvim/lua/user/remap.lua
@@ -2,3 +2,9 @@ local nnoremap = require("user.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 
+
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+
