c268bcf661ab4684ce45b1070ca84f28d7483c88
Paweł Ostrowski <pasza@pasza.org>
2023-10-25 11:52:09 +0200

    nvim elixir lsp setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -438,6 +438,10 @@ lspconfig.ocamllsp.setup {
 	end,
 	capabilities = ocaml_capabilities,
 }
+require('lspconfig').elixirls.setup {
+	-- cmd = { "/home/pasza/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
+	on_attach = lsp_on_attach,
+}
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
