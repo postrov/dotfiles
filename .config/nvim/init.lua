87eacfd3fb1af3ee7fbd42fe2940d00e6f32ca07
Pawel Ostrowski <pasza@pasza.org>
2024-02-22 16:48:13 +0100

    nvim: disabled obsidian plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -102,39 +102,39 @@ local cmpSetup = function()
 	}
 end
 require("lazy").setup({
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		--   "BufReadPre path/to/my-vault/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
-		-- },
-		dependencies = {
-			-- Required.
-			"nvim-lua/plenary.nvim",
-
-			-- see below for full list of optional dependencies 👇
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "it",
-					path = "~/GoogleDrive/DriveSyncFiles/IT Knowledge",
-				},
-				{
-					name = "bible",
-					path = "~/GoogleDrive/DriveSyncFiles/Bible Study PL",
-				},
-			},
-
-			-- see below for full list of options 👇
-		},
-	},
+	-- {
+	-- 	"epwalsh/obsidian.nvim",
+	-- 	version = "*", -- recommended, use latest release instead of latest commit
+	-- 	lazy = true,
+	-- 	ft = "markdown",
+	-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- 	-- event = {
+	-- 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	-- 	--   "BufReadPre path/to/my-vault/**.md",
+	-- 	--   "BufNewFile path/to/my-vault/**.md",
+	-- 	-- },
+	-- 	dependencies = {
+	-- 		-- Required.
+	-- 		"nvim-lua/plenary.nvim",
+	--
+	-- 		-- see below for full list of optional dependencies 👇
+	-- 	},
+	-- 	opts = {
+	-- 		workspaces = {
+	-- 			{
+	-- 				name = "it",
+	-- 				path = "~/GoogleDrive/DriveSyncFiles/IT Knowledge",
+	-- 			},
+	-- 			{
+	-- 				name = "bible",
+	-- 				path = "~/GoogleDrive/DriveSyncFiles/Bible Study PL",
+	-- 			},
+	-- 		},
+	--
+	-- 		-- see below for full list of options 👇
+	-- 	},
+	-- },
 	{
 		"tpope/vim-fugitive"
 
