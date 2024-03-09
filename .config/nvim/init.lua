88e225de526b66a081ec13224980ab909cfea2eb
Pawel Ostrowski <pasza@pasza.org>
2024-03-09 18:53:52 +0100

    nvim: uv -> loop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,7 @@ vim.o.foldenable = false
 -- Snippet taken from
 -- https://github.com/folke/lazy.nvim#-installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
+if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
