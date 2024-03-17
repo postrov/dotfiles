dd480f605b6b44fa14b81ec2621f552ce99d5dab
Pawel Ostrowski <pasza@pasza.org>
2024-03-18 00:31:39 +0100

    emacs: evil config wip

--- .config/emacs_new/early-init.el
+++ .config/emacs_new/early-init.el
@@ -1,5 +1,6 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-message t)
 ;; do not start default package manager
 ;(setq package-enable-at-startup nil)
--- .config/emacs_new/init.el
+++ .config/emacs_new/init.el
@@ -13,9 +13,9 @@
 (eval-when-compile (require 'use-package))
 
 ;;; Quelpa
-(use-package quelpa)
-(use-package quelpa-use-package)
-(quelpa-use-package-activate-advice)
+;; (use-package quelpa)
+;; (use-package quelpa-use-package)
+;; (quelpa-use-package-activate-advice)
 (setq load-prefer-newer t)
 
 ;;; Vim Bindings
@@ -27,10 +27,11 @@
   ;; (setq evil-search-module 'evil-search)
   (setq evil-want-keybinding nil)
   ;; no vim insert bindings
-  ;;(setq evil-undo-system 'undo-fu)
+  (setq evil-undo-system 'undo-fu)
   :config
   (evil-mode 1))
 
+(use-package undo-fu)
 
 ;;; Vim Bindings Everywhere else
 (use-package evil-collection
@@ -71,15 +72,19 @@
 
 
 ;;; Appearance
-(defun my-setup-color-theme ()
-  (interactive)
-  (when (display-graphic-p)
-    (load-theme (car modus-themes-to-toggle))))
+;; when display-graphic-p
 
-(use-package modus-themes
-  :quelpa (modus-themes :fetcher github :repo "protesilaos/modus-themes")
-  :init (setq modus-themes-to-toggle '(modus-vivendi modus-operandi))
-  :config (my-setup-color-theme))
+(defun my-theme-settings ()
+  (load-theme 'catppuccin :no-confirm)
+  (if (display-graphic-p)
+      (progn
+	(set-frame-parameter nil 'alpha-background 90)
+	(set-frame-font "Cascadia Code PL-13"))
+    (progn (set-face-background 'default "undefined")
+	   (set-face-background 'line-number "undefined"))))
+
+(use-package catppuccin-theme
+  :init (my-theme-settings))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -87,12 +92,81 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0f76f9e0af168197f4798aba5c5ef18e07c926f4e7676b95f2a13771355ce850" default))
+   '("80214de566132bf2c844b9dee3ec0599f65c5a1f2d6ff21a2c8309e6e70f9242" "0f76f9e0af168197f4798aba5c5ef18e07c926f4e7676b95f2a13771355ce850" default))
  '(package-selected-packages
-   '(evil-collection cmake-mode quelpa-use-package modus-themes evil)))
+   '(catpuccin-theme catppuccin-theme rust-mode dap-mode lsp-ui lsp-mode slime-company slime go-mode affe orderless vertico company-prescient evil-collection cmake-mode quelpa-use-package modus-themes evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(evil-set-leader 'normal (kbd "SPC"))
+(evil-define-key 'normal 'global (kbd "<leader>SPC") 'switch-to-buffer)
+
+(use-package vertico :config (vertico-mode +1))
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (Completion-category-overrides '((file (styles basic partial-completion)))))
+(use-package prescient :config (prescient-persist-mode +1))
+(use-package company-prescient :init (company-prescient-mode +1))
+
+(use-package affe)
+
+(evil-define-key 'normal 'global (kbd "<leader>p") 'affe-find)
+(evil-define-key 'normal 'global (kbd "<leader>s g") 'affe-grep)
+
+;; TODO: load only for go
+(use-package go-mode)
+
+;; TODO: load only on lisp
+(use-package slime)
+;; TODO: SETUP
+(use-package slime-company)
+
+
+;; (use-package lsp-mode
+;; ;  :if my-laptop-p
+;;   :config
+;;   (setq lsp-headerline-breadcrumb-enable t
+;;         gc-cons-threshold (* 100 1024 1024)
+;;         read-process-output-max (* 1024 1024)
+;;         company-idle-delay 0.5
+;;         company-minimum-prefix-length 1
+;;         create-lockfiles nil ;; lock files will kill `npm start'
+;;         )
+;; ;  (lsp-register-custom-settings
+;; ;   '(("pyls.plugins.pyls_mypy.enabled" t t)
+;; ;     ("pyls.plugins.pyls_mypy.live_mode" nil t)
+;; ;     ("pyls.plugins.pyls_black.enabled" t t)
+;; ;     ("pyls.plugins.pyls_isort.enabled" t t)))
+;; ;  :hook ((prog-mode-hook . lsp)
+;; ;         (python-mode . lsp)
+;; ;         (lsp-mode-hook . lsp-enable-which-key-integration))
+;;   :commands lsp
+;;   :after ht
+;;   ;; :vc (:url "https://github.com/emacs-lsp/lsp-mode"
+;;   ;;      :branch "13f400b4f108b6286089764b3efad082c67f72ed")
+;;   )
+
+;; (use-package lsp-ui
+;;   :commands lsp-ui-mode
+;;   :after lsp-mode)
+
+;; (use-package dap-mode
+;;   :after lsp-mode)
+
+
+;; (use-package rust-mode
+;;   :mode ("\\.rust$" . rust-mode)
+;;   :commands (rust-mode)
+;;   :config
+;;   ;; install rustfmt using `cargo install rustfmt'
+;;   (when (executable-find "rustfmt")
+;;     (add-hook 'rust-mode-hook
+;;               (lambda ()
+;;                 (add-hook 'before-save-hook
+;;                           (lambda ()
+;;                             (rust-format-buffer)) nil t)))))  
